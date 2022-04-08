f73cc09ce553fa61481b47af77cb5647cc810018
gabriele2017 <33287825+gabriele2017@users.noreply.github.com>
2022-04-08 12:50:02 +0100

    Add files via upload

---
+++ run_stage1.sh
@@ -0,0 +1,1 @@
+~/HIC/ICeCap/ICeCAP -J 12 -l test -H mm9 -G 0 -Z 1 -C 0 -R ../Downloads/ES/SRR2240730_1.fastq -L ../Downloads/ES/SRR2240730_2.fastq -P ~/HIC/ICeCap/reference_bowtie_mm9/ -f ~/HIC/ICeCap/ 
---
+++ run_stage2.sh
@@ -0,0 +1,7 @@
+export PATH="$PATH:/apps/well/R/3.4.3/lib64/R/bin/";
+export PATH="$PATH:/well/jknight/gabriele/JUICER/juicer/scripts/common/"
+install(peakY)
+
+~/ICeCap/ICeCAP -E GATC -N stage2 -l CaptureC_CD14_Rep1 -B ~/ICeCap/baits.bed -Q ~/ICeCap/reference_map_mm9/ -S ziw -C 0 -G 0 -Z 1 -M 1 -P ~/ICeCap/reference_bowtie_mm9/ -f ~/ICeCap/
+
+
